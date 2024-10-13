ea20191524002d91ce79de70811e5a33a7e2806d
Hot-ao <gkxogh02@naver.com>
2024-10-13 20:52:47 +0900

    [Bronze III] Title: 팩토리얼, Time: 168 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+    	int num = sc.nextInt();
+    	int total=1;
+    	if (num == 0)
+    		total = 1;
+    	else
+    		for (int i=1;i<num+1;i++) total*=i;
+    	System.out.println(total);
+
+    }	
+}
