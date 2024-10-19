25d3fc126ff053dc5bf3f27cb89d964f2d289835
Hot-ao <gkxogh02@naver.com>
2024-10-20 00:26:29 +0900

    [Bronze III] Title: 직각삼각형, Time: 184 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+	public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			double a = sc.nextDouble();
+			double b = sc.nextDouble();
+			double c = sc.nextDouble();
+			double temp;
+			if (a==0&&b==0&&c==0) break;
+			if (a<b) {
+				temp=b;
+				b=a;
+				a=temp;
+			}
+			if (a<c) {
+				temp=a;
+				a=c;
+				c=temp;
+			}
+			if (b<c) {
+				temp=b;
+				b=c;
+				c=temp;
+			}
+			if(a*a==(b*b+c*c)) System.out.println("right");
+			
+			else System.out.println("wrong");
+		}
+	}
+}
