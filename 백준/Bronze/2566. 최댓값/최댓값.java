3945d9f8d4d8ef8c1538ec94a9efed9e313aa84b
Hot-ao <gkxogh02@naver.com>
2024-10-21 21:25:24 +0900

    [Bronze III] Title: 최댓값, Time: 192 ms, Memory: 18456 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+	public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int table[][] = new int [9][9];
+		int row=0;
+		int col=0;
+		int max=0;
+		
+		for (int i=0;i<table.length;i++) {
+			for (int j=0;j<table[i].length;j++) {
+				table[i][j] = sc.nextInt();
+			}
+		}
+		
+		for (int i=0;i<table.length;i++) {
+			for (int j=0;j<table[i].length;j++) {
+				if(table[i][j]>=max) {
+					max = table[i][j];
+					row = i+1;
+					col = j+1;
+				}
+			}
+		}
+		System.out.println(max);
+		System.out.println(row+" "+col);
+	}
+}
